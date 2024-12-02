1b3a56cc95c9bcc0fb8493456ffbcd9a4e316c21
Sandman6z <zbn0238@163.com>
2024-12-02 20:00:08 +0800

    Neb when power on

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,14 +128,15 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   //  sweepFreq();
   // 扫频完成后，设置TIM1为最佳频�??
-   LED_Init();
-
+//   LED_Init();
+__disable_irq();  
   while (1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    CheckSignalTimeout();
+    //CheckSignalTimeout();
+      startNeb();
       
       
   }
