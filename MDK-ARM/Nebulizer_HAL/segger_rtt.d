68ecaf3058176ccff3068821d118b164b4b1f21f
Sandman6z <zbn0238@163.com>
2024-12-04 16:27:03 +0800

    Move ./Middlewares folder to .\Drivers\Middlewares Folders are no longer purged after regenerating with cubeMX

--- MDK-ARM/Nebulizer_HAL/segger_rtt.d
+++ MDK-ARM/Nebulizer_HAL/segger_rtt.d
@@ -1,6 +1,7 @@
-nebulizer_hal/segger_rtt.o: ..\Middlewares\SEGGER_RTT\SEGGER_RTT.c \
-  ..\Middlewares\SEGGER_RTT\SEGGER_RTT.h \
-  ..\Middlewares\SEGGER_RTT\SEGGER_RTT_Conf.h \
+nebulizer_hal/segger_rtt.o: \
+  ..\Drivers\Middlewares\SEGGER_RTT\SEGGER_RTT.c \
+  ..\Drivers\Middlewares\SEGGER_RTT\SEGGER_RTT.h \
+  ..\Drivers\Middlewares\SEGGER_RTT\SEGGER_RTT_Conf.h \
   C:\Program\ Files\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
   C:\Program\ Files\Keil_v5\ARM\ARMCLANG\include\stdarg.h \
   C:\Program\ Files\Keil_v5\ARM\ARMCLANG\include\stdint.h \
--- MDK-ARM/Nebulizer_HAL/segger_rtt_printf.d
+++ MDK-ARM/Nebulizer_HAL/segger_rtt_printf.d
@@ -1,7 +1,7 @@
 nebulizer_hal/segger_rtt_printf.o: \
-  ..\Middlewares\SEGGER_RTT\SEGGER_RTT_printf.c \
-  ..\Middlewares\SEGGER_RTT\SEGGER_RTT.h \
-  ..\Middlewares\SEGGER_RTT\SEGGER_RTT_Conf.h \
+  ..\Drivers\Middlewares\SEGGER_RTT\SEGGER_RTT_printf.c \
+  ..\Drivers\Middlewares\SEGGER_RTT\SEGGER_RTT.h \
+  ..\Drivers\Middlewares\SEGGER_RTT\SEGGER_RTT_Conf.h \
   C:\Program\ Files\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
   C:\Program\ Files\Keil_v5\ARM\ARMCLANG\include\stdarg.h \
   C:\Program\ Files\Keil_v5\ARM\ARMCLANG\include\stdint.h
